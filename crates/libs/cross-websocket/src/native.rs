ae3bc081cfb005628f32e92ee218e153ac6051a7
Ryo Hirayama <ryo12redstone@gmail.com>
2022-02-09 17:30:37 +0900

    [cross-websocket] Add Rx Tx types

--- crates/libs/cross-websocket/src/native.rs
+++ crates/libs/cross-websocket/src/native.rs
@@ -1,33 +1,55 @@
 use std::borrow::Cow;
 
-use tokio_tungstenite::connect_async;
-use tokio_tungstenite::tungstenite::Message;
+use futures::sink::SinkMapErr;
+use futures::stream::{Map, SplitSink, SplitStream};
+use tokio::net::TcpStream;
+use tokio_tungstenite::tungstenite::{Error, Message};
+use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
 
 use crate::{SinkError, WebSocketClient};
 
 use eyre::Result;
 use futures::future::{ready, Ready};
-use futures::{prelude::*, Sink, Stream};
+use futures::prelude::*;
 
 type Bytes = Vec<u8>;
 
-pub async fn connect<'a, T: Into<Cow<'a, str>>>(
-    addr: T,
-) -> Result<
-    WebSocketClient<
-        impl Sink<Bytes, Error = SinkError> + Send + Sync + Unpin + 'static,
-        impl Stream<Item = Result<Bytes>> + Send + Sync + Unpin + 'static,
+pub type Rx = Map<
+    SplitStream<WebSocketStream<MaybeTlsStream<tokio::net::TcpStream>>>,
+    fn(Result<Message, Error>) -> Result<Bytes>,
+>;
+
+pub type Tx = sink::With<
+    SinkMapErr<
+        SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, Message>,
+        fn(Error) -> SinkError,
     >,
-> {
+    Message,
+    Bytes,
+    Ready<Result<Message, SinkError>>,
+    fn(Bytes) -> Ready<Result<Message, SinkError>>,
+>;
+
+pub async fn connect<'a, T: Into<Cow<'a, str>>>(addr: T) -> Result<WebSocketClient<Tx, Rx>> {
     let str: Cow<'a, str> = addr.into();
     let (stream, _response) = connect_async(str.as_ref()).await?;
     let (tx, rx) = stream.split();
-    let rx = rx.map(|message| -> Result<Bytes> { Ok(message?.into_data()) });
+    let rx = rx.map(bytes as fn(_) -> _);
     let tx = tx
-        .sink_map_err(|err| SinkError::Send(err.to_string()))
-        .with(|bytes: Bytes| -> Ready<Result<Message, SinkError>> {
-            ready(Ok(Message::binary(bytes)))
-        });
+        .sink_map_err(err as fn(_) -> _)
+        .with(message as fn(_) -> _);
 
     Ok(WebSocketClient { tx, rx })
+}
+
+fn bytes(message: Result<Message, Error>) -> Result<Bytes> {
+    Ok(message?.into_data())
+}
+
+fn err(err: Error) -> SinkError {
+    SinkError::Send(err.to_string())
+}
+
+fn message(bytes: Bytes) -> Ready<Result<Message, SinkError>> {
+    ready(Ok(Message::binary(bytes)))
 }
--- crates/libs/cross-websocket/src/wasm.rs
+++ crates/libs/cross-websocket/src/wasm.rs
@@ -1,30 +1,44 @@
+use futures::sink::{SinkMapErr, With};
+use futures::stream::{Map, SplitSink, SplitStream};
 use std::borrow::Cow;
-use ws_stream_wasm::{WsMessage, WsMeta};
+use ws_stream_wasm::{WsErr, WsMessage, WsMeta, WsStream};
 
 use crate::{SinkError, WebSocketClient};
 use eyre::Result;
 use futures::future::{ready, Ready};
-use futures::{prelude::*, Sink, Stream};
+use futures::prelude::*;
 
 type Bytes = Vec<u8>;
 
-pub async fn connect<'a, T: Into<Cow<'a, str>>>(
-    addr: T,
-) -> Result<
-    WebSocketClient<
-        impl Sink<Bytes, Error = SinkError> + Send + Sync + Unpin + 'static,
-        impl Stream<Item = Result<Bytes>> + Send + Sync + Unpin + 'static,
-    >,
-> {
+type Rx = Map<SplitStream<WsStream>, fn(WsMessage) -> Result<Bytes>>;
+type Tx = With<
+    SinkMapErr<SplitSink<WsStream, WsMessage>, fn(WsErr) -> SinkError>,
+    WsMessage,
+    Bytes,
+    Ready<Result<WsMessage, SinkError>>,
+    fn(Bytes) -> Ready<Result<WsMessage, SinkError>>,
+>;
+
+pub async fn connect<'a, T: Into<Cow<'a, str>>>(addr: T) -> Result<WebSocketClient<Tx, Rx>> {
     let str: Cow<'a, str> = addr.into();
     let (_, wsio) = WsMeta::connect(str.as_ref(), None).await?;
     let (tx, rx) = wsio.split();
-    let rx = rx.map(|message| -> Result<Bytes> { Ok(message.into()) });
+    let rx = rx.map(bytes as fn(_) -> _);
     let tx = tx
-        .sink_map_err(|err| SinkError::Send(err.to_string()))
-        .with(|bytes: Bytes| -> Ready<Result<WsMessage, SinkError>> {
-            ready(Ok(WsMessage::Binary(bytes)))
-        });
+        .sink_map_err(err as fn(_) -> _)
+        .with(message as fn(_) -> _);
 
     Ok(WebSocketClient { tx, rx })
+}
+
+fn bytes(message: WsMessage) -> Result<Bytes> {
+    Ok(message.into())
+}
+
+fn err(err: WsErr) -> SinkError {
+    SinkError::Send(err.to_string())
+}
+
+fn message(bytes: Bytes) -> Ready<Result<WsMessage, SinkError>> {
+    ready(Ok(WsMessage::Binary(bytes)))
 }
