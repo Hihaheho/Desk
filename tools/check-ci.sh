c6a81331114c773591ffa5b645d12db0af9322db
Ryo Hirayama <ryo12redstone@gmail.com>
2022-05-09 11:57:40 +0900

    Fix CI

--- tools/check-ci.sh
+++ tools/check-ci.sh
@@ -1,10 +1,19 @@
 #! /usr/bin/env bash
 
 # fail fast
-set -euo pipefail
+set -eo pipefail
 shopt -s inherit_errexit
 
-cargo deny check --config configs/deny.toml
+for OPT in "$@"
+do
+    case $OPT in
+        --no-cargo-deny)
+	    NO_CARGO_DENY=1
+            ;;
+    esac
+done
+
+[[ -z ${NO_CARGO_DENY} ]] && cargo deny check --config configs/deny.toml
 cargo fmt --all -- --check
 cargo clippy --all-targets --all-features -- -D warnings -W clippy::all -W clippy::dbg_macro
 cargo check --all-features
