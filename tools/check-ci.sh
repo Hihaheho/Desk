3d8aea3e4a28e1fd0c6e21abd85f63bedec7005c
Ryo Hirayama <ryo12redstone@gmail.com>
2022-05-09 10:19:52 +0900

    Use tools/check-ci.sh in CI

--- tools/check-ci.sh
+++ tools/check-ci.sh
@@ -4,8 +4,9 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
-cargo fmt
 cargo deny check --config configs/deny.toml
+cargo fmt --all -- --check
 cargo clippy --all-targets --all-features -- -D warnings -W clippy::all -W clippy::dbg_macro
 cargo check --all-features
+cargo test --no-run --locked --all-features
 cargo test --all-features
