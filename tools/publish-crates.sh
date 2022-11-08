669b7b22f65b911cf86852c6a75a205900a3b5fc
Ryo Hirayama <ryo12redstone@gmail.com>
2022-11-08 15:42:39 +0900

    Make tools/publish-crates.sh more smart

--- tools/publish-crates.sh
+++ tools/publish-crates.sh
@@ -1,12 +1,31 @@
 #! /usr/bin/env bash
 
+# fail fast
+set -euo pipefail
+shopt -s inherit_errexit
+
 VERSION=0.0.0
 
+skip_until=${1:-""}
+skip="yes"
+if [ -z $skip_until ]; then
+    skip="no"
+fi
+
 tools/list-crates.sh | while read crate; do
+    if [ $skip = "yes" ]; then
+        if [ $crate = $skip_until ]; then
+            skip="no"
+        else
+            continue
+        fi
+    fi
     if cargo search ${crate} | grep --quiet "${crate} = \"${VERSION}\""; then
         echo "Skipping ${crate} because it is already published"
+        sleep 5
     else
-        echo "Publishing ${crate}" &&
-        cargo publish -p ${crate} --no-verify && sleep 20
+        echo "Publishing ${crate}"
+        cargo publish -p ${crate} --no-verify
+        sleep 20
     fi
 done
