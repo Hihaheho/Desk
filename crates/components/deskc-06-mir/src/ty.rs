170f34f07fcf7eb2a3a8bb9176890deffad6cc13
Ryo Hirayama <ryo12redstone@gmail.com>
2022-11-12 16:03:32 +0900

    [deskvm] Remove MIR and rename AMIR to MIR

--- crates/components/deskc-06-mir/src/ty.rs
+++
@@ -1,3 +0,0 @@
-pub type Id = usize;
-
-pub struct ConcTypeId(pub usize);
--- crates/components/deskc-conc-types/src/region.rs
+++
@@ -1,4 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
-pub struct RegionId(pub usize);
